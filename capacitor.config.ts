c06fba1cc1e8a3eea289d2ae46a3be36b8110d90
DavidBistron <154263696+DavidBistron@users.noreply.github.com>
2024-11-30 12:41:06 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.recipewebapp',
+  appName: 'ionic-recipewebapp',
+  webDir: 'www'
+};
+
+export default config;
