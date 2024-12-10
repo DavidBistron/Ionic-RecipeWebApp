0bc0dbe08804cd52b2ed2e66b2d0bf213b51f4e3
DavidBistron <154263696+DavidBistron@users.noreply.github.com>
2024-12-10 10:00:20 +0100

    Header defined globally in app.component.html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonIcon],
 })
 export class AppComponent {
   constructor() {}
