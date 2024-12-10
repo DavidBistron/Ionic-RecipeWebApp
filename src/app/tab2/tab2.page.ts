a50f3eb4634c291d7e39c1966b0e5d5a6e5227d1
DavidBistron <154263696+DavidBistron@users.noreply.github.com>
2024-12-10 20:39:44 +0100

    Improved design; Added google font

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonIcon, IonToolbar, IonTitle, IonContent, IonAccordionGroup, IonAccordion, IonItem, IonLabel, IonChip, IonFab, IonFabButton, IonFabList, IonModal, IonButton, IonButtons, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonContent]
+  imports: [ IonHeader, IonIcon, IonToolbar, IonTitle, IonContent, IonAccordionGroup, IonAccordion, IonItem, IonLabel, IonChip, IonFab, IonFabButton, IonFabList, IonModal, IonButton, IonButtons, IonInput],
 })
 export class Tab2Page {
 
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonIcon, IonToolbar, IonTitle, IonContent, IonAccordionGroup, IonAccordion, IonItem, IonLabel, IonChip, IonFab, IonFabButton, IonFabList, IonModal, IonButton, IonButtons, IonInput } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonContent],
+  imports: [ IonHeader, IonIcon, IonToolbar, IonTitle, IonContent, IonAccordionGroup, IonAccordion, IonItem, IonLabel, IonChip, IonFab, IonFabButton, IonFabList, IonModal, IonButton, IonButtons, IonInput],
 })
 export class Tab3Page {
 
